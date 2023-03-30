6401b36461913f9a26a53b508ce0979f262e7a78
DragonFlameStrike <d.pankov@g.nsu.ru>
2023-03-30 14:54:14 +0700

    Починил CORS политику

--- bd_zoo/backend/src/main/java/com/pankov/bd_zoo/BdZooApplication.java
+++ bd_zoo/backend/src/main/java/com/pankov/bd_zoo/BdZooApplication.java
@@ -2,6 +2,9 @@ package com.pankov.bd_zoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.stream.IntStream;
 
@@ -10,6 +13,19 @@ public class BdZooApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BdZooApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000", "http://localhost")
+						.allowCredentials(true)
+						.allowedMethods("GET", "POST", "UPDATE", "DELETE", "PUT");
+			}
+		};
 	}
 
 }
