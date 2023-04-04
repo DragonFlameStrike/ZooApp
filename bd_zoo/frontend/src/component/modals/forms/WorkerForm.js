77ed6fab6953af8f54ca685f2891f565a99805f1
DragonFlameStrike <d.pankov@g.nsu.ru>
2023-04-04 17:55:57 +0700

    Добавил возомжность нанять новых работников. Создал пул воркеров котоырй хранит рандомносгенерированных воркеров для найма

--- bd_zoo/frontend/src/component/modals/forms/WorkerForm.js
+++ bd_zoo/frontend/src/component/modals/forms/WorkerForm.js
@@ -1,21 +1,65 @@
-import React from 'react';
-import { Routes ,Route } from 'react-router-dom';
-import {Col, Form, Label, Row} from "reactstrap";
-
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Row, Col, Label, Button } from 'reactstrap';
 
 const WorkerForm = (props) => {
+
+    const [worker, setWorker] = useState([]);
+
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/workers/pool/' + props.id)
+            .then(response => {
+                setWorker(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    const handleButtonClick = () => {
+        axios.post(`http://localhost:8080/workers/pool/${props.id}`, {})
+            .then(response => {
+                props.toggle();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
     return (
         <Form>
-            <Row>
-                <Col className="col-12">
+            {worker && (
+                <>
                     <div className="mb-3">
-                        <Label className="form-label">Контактные данные:</Label>
-                        {"89132017838"}
+                        <Label className="form-label">Имя:</Label>
+                        {worker.name}
                     </div>
-                </Col>
-            </Row>
+                    <div className="mb-3">
+                        <Label className="form-label">Пол:</Label>
+                        {worker.sex}
+                    </div>
+                    <div className="mb-3">
+                        <Label className="form-label">Профессия:</Label>
+                        {worker.profession}
+                    </div>
+                    <div className="mb-3">
+                        <Label className="form-label">Дата рождения:</Label>
+                        {worker.birthday}
+                    </div>
+                    <div className="mb-3">
+                        <Label className="form-label">Стаж:</Label>
+                        {worker.priorService}
+                    </div>
+                    <div className="mb-3">
+                        <Label className="form-label">Зарплата:</Label>
+                        {worker.salary}
+                    </div>
+                    <Button color="primary" onClick={handleButtonClick}>Нанять</Button>
+                </>
+            )}
         </Form>
     );
 }
+
 export default WorkerForm;
